713df086b918cdbf115ee04d8e075614f4362e32
aryansh-2003 <dixitsanjeev1975@gmail.com>
2025-10-10 20:14:04 +0530

    d

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ connectDB()
         throw error
     })
     
-    app.get(process.env.PORT || 8000,()=>{
+    app.listen(process.env.PORT || 8000,()=>{
         console.log(`server is running at ${process.env.PORT}`  )
     })
 })
