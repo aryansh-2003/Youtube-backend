f1a965ff0311ee1c3418cc52e9550d89d665a79c
aryansh-2003 <dixitsanjeev1975@gmail.com>
2025-10-10 20:32:40 +0530

    Connected frontend

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: [process.env.CORS_ORIGIN ,"http://localhost:5173"],
+    origin: [process.env.CORS_ORIGIN ,"https://yotube-iota.vercel.app/"],
     credentials: true
 }))
 
