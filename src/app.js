e383a15b4eedf0c918cb708c77c8e2c5c11710c9
aryansh-2003 <dixitsanjeev1975@gmail.com>
2025-10-10 20:20:58 +0530

    Done

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: [process.env.CORS_ORIGIN || 1000],
+    origin: [process.env.CORS_ORIGIN ,"http://localhost:5173"],
     credentials: true
 }))
 
