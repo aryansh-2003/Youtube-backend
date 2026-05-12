a26eb8faaea400f890e49e1c8581b96b3ac15c2c
aryansh-2003 <dixitsanjeev1975@gmail.com>
2026-05-12 22:44:05 +0530

    Address the http cookie behaviour

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -123,7 +123,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
-        sameSite: "Lax",
+        sameSite: "None",
     }
 
     return res
@@ -147,7 +147,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
-        sameSite: "Lax"
+        sameSite: "None"
     }
 
     return res
